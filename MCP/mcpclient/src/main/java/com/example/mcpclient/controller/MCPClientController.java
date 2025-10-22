3bb27cad278bbc89a449cedd62b468dfb2fed700
gunasekharp <gunasekharp22066@gmail.com>
2025-10-22 14:53:23 +0530

    RequestHeader included

--- MCP/mcpclient/src/main/java/com/example/mcpclient/controller/MCPClientController.java
+++ MCP/mcpclient/src/main/java/com/example/mcpclient/controller/MCPClientController.java
@@ -3,10 +3,7 @@ package com.example.mcpclient.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,7 +18,8 @@ public class MCPClientController {
     }
 
     @GetMapping("/chat")
-    public String chat(@RequestParam("message") String message){
+    public String chat(@RequestHeader(value = "username",required = false) String username,
+                       @RequestParam("message") String message){
         return chatClient.prompt().user(message).call().content();
     }
 }
