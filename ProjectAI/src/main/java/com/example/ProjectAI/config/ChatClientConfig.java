af26921f828665cc32a8652b8e0268622cf42900
gunasekharp <gunasekharp22066@gmail.com>
2025-10-23 14:02:03 +0530

    Included Chat Options

--- ProjectAI/src/main/java/com/example/ProjectAI/config/ChatClientConfig.java
+++ ProjectAI/src/main/java/com/example/ProjectAI/config/ChatClientConfig.java
@@ -3,6 +3,7 @@ package com.example.ProjectAI.config;
 import com.example.ProjectAI.Advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,10 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClient){
+        ChatOptions chatOptions=ChatOptions.builder().temperature(0.8).maxTokens(250)
+                .topK(3).build();
         return chatClient
+                .defaultOptions(chatOptions)
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(),new TokenUsageAuditAdvisor()))
                 .defaultSystem("""
                         You are an internal IT helpdesk assistant. Your role is to assist 
